449c74d765a01f17d11f138e707aa7726283146c
Joseph Dailey <josephldialey@gmail.com>
2016-12-31 23:36:36 +0000

    Build script is more informative. npm issue  is resolved

--- build.sh
+++ build.sh
@@ -5,41 +5,41 @@
 pwd=$(pwd)
 ind="/opt/khk-web"
 if [ "$pwd" != "$ind" ]; then
-	echo Projects installed to the wrong directory.
-	echo Projects must be installed to /opt/khk-web.
+	echo !! Projects installed to the wrong directory.
+	echo !! Projects must be installed to /opt/khk-web.
 	exit 1
 fi
 
 	
 if [ ! -d /opt/cert ]; then
-	echo Missing /opt/cert/. These files are crucial for SSO with Google Drive.
+	echo !! Missing /opt/cert/. These files are crucial for SSO with Google Drive.
 	exit 1
 fi
 
-	
+
 if [ ! -d /opt/tls ]; then
-	echo Missing /opt/tls/. These files are crucial for HTTPS configuration.
+	echo !! Missing /opt/tls/. These files are crucial for HTTPS configuration.
 	exit 1
 fi
 
-echo Veryifying repo\(s\) are up to date.
+echo ::  Veryifying repo\(s\) are up to date.
 git pull
 git submodule init
 git submodule update
 
 #custom nginx conf
-echo Installing main Nginx configuration
+echo :: Installing main Nginx configuration
 if ! which nginx > /dev/null 2>&1; then
-	echo "Nginx not installed. Install and retry"
+	echo !! "Nginx not installed. Install and retry"
 	exit 1
 fi
-sudo cp ./nginx/nginx.conf /etc/nginx/.
-sudo mkdir /etc/nginx/sites
+sudo cp ./nginx/nginx.conf /etc/nginx/. -v
+sudo mkdir /etc/nginx/sites -v
 
 #sub-project installation and configuration
 for D in `find . -maxdepth 1 -name "khk-*" -type d`
 do
-	echo Installing configuration for ${D#./}
+	echo :: Installing Nginx configuration for ${D#./}
 	if cd ${D}; then
 		if /bin/bash build.sh; then
 			cd cp
@@ -47,12 +47,13 @@ do
 			site=$(find . -maxdepth 1 -name "*.site")
 			echo $site
 			if [ -f $site ]; then
-				sudo cp $site /etc/nginx/sites/.
+				sudo cp $site /etc/nginx/sites/. -v
 		        else
-				echo ${D#./} is missing a NGINX .site config. Please see the standards in the README.
+				echo !! ${D#./} is missing a NGINX .site config. Please see the standards in the README.
 				exit 1
 			fi
-			echo Testing NGINX site config for ${D#./}. \(Previously loaded sites may have already broken NGINX\) 
+			echo :: Testing updated NGINX site config for ${D#./}.
+			echo ::: \(Previously loaded sites may have already broken NGINX\) 
 			sudo nginx -t
 			service=$(find . -maxdepth 1 -name "*.service")
 		        if [ -f $service ]; then
@@ -61,17 +62,25 @@ do
 				sudo systemctl enable ${service#./}
 				sudo systemctl start ${service#./}
 		        else
-		                echo Project located at ${D} is missing a systemd .service file. Please see the standards in the README.
+		                echo !! Project located at ${D} is missing a systemd *.service file.
+										echo !!! Please see the standards in the README.
 		                exit 1
 		        fi
-			echo ${D#./} has been successfully install. Please verify successfull operation:
+			echo ""
+			echo ""
+			echo ?? ${D#./} has been successfully installed. Please verify successfull operation:
+			echo ???  \(press \'q\' to continue\)
+			echo ""
+			echo ""
 			sudo systemctl status ${service#./}
+			echo ""
+			echo ""
 			cd ../../
 		else
 			exit 1
 		fi
 	else
-		echo ${D#./} does not have a cp directory. Please see the standards in the README.
+		echo !! ${D#./} does not have a cp directory. Please see the standards in the README.
 	fi
 
 	sudo systemctl restart nginx.service
